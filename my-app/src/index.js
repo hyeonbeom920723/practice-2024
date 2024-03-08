15c23d3cd356d62038258cbd1dea18ab4cec932a
hyeonbeom.lee <hyeonbeom920723@bitbucket.org>
2024-03-07 20:27:18 -0700

    component and prop practice

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,17 +5,14 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 import Clock from "./chapter_04/Clock";
+import CommentList from "./chapter_05/CommentList";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-setInterval(() => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Clock />
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-}, 1000);
+ReactDOM.render(
+  <React.StrictMode>
+    <CommentList />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
